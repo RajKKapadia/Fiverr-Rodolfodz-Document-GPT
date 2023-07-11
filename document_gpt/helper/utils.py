47994329caae5755520f504eaad72631baf773b6
RajKKapadia <raajforyou@gmail.com>
2023-07-11 14:11:55 +0530

    added audio transcript function

---
+++ document_gpt/helper/utils.py
@@ -0,0 +1,34 @@
+import os
+
+import openai
+import soundfile as sf
+import requests
+import uuid
+
+from config import config
+
+def transcript_audio(media_url: str) -> dict:
+    try:
+        ogg_file_path = f'{config.OUTPUT_DIR}/{uuid.uuid1()}.ogg'
+        data = requests.get(media_url)
+        with open(ogg_file_path, 'wb') as file:
+            file.write(data.content)
+        audio_data, sample_rate = sf.read(ogg_file_path)
+        mp3_file_path = f'{config.OUTPUT_DIR}/{uuid.uuid1()}.mp3'
+        sf.write(mp3_file_path, audio_data, sample_rate)
+        audio_file = open(mp3_file_path, 'rb')
+        os.unlink(ogg_file_path)
+        os.unlink(mp3_file_path)
+        transcript = openai.Audio.transcribe(
+            'whisper-1', audio_file, api_key=config.OPENAI_API_KEY)
+        return {
+            'status': 1,
+            'transcript': transcript['text']
+        }
+    except Exception as e:
+        print('Error at transcript_audio...')
+        print(e)
+        return {
+            'status': 0,
+            'transcript': transcript['text']
+        }
